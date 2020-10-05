fdb72b7ccaba5c461e4fd65bce6ad6154d2b869c
williamv1 <wmvanhook@gmail.com>
2020-10-05 18:05:51 -0400

    Add show route

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -7,6 +7,15 @@ router.get('/', (req,res) => {
     res.render('exercises/index', {exercises: exercises})
 });
 
+//show route
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const exercise = exercises[id];
+
+    res.render('exercises/show', {exercise: exercise})
+
+});
+
 
 
 module.exports = router;
