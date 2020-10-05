a40a5b672950b3c3875c62d50c4374036d72621e
williamv1 <wmvanhook@gmail.com>
2020-10-05 17:10:45 -0400

    Add temporary data to model

---
+++ models/Exercise.js
@@ -0,0 +1,41 @@
+const exercises = [{
+    name: 'push ups',
+    equipment: 'body',
+    reps: 10,
+    sets: 3,
+    weight: 160,
+    completed: true
+},
+{
+    name: 'squat',
+    equipment: 'barbell',
+    reps: 6,
+    sets: 3,
+    weight: 215,
+    completed: false
+},
+{
+    name: 'bicep curls',
+    equipment: 'dumbbells',
+    reps: 12,
+    sets: 3,
+    weight: 60,
+    completed: true
+},
+{
+    name: 'bench press',
+    equipment: 'barbell',
+    reps: 6,
+    sets: 5,
+    weight: 145,
+    completed: true
+},
+{
+    name: 'deadlift',
+    equipment: 'barbell',
+    reps: 5,
+    sets: 3,
+    weight: 225,
+    completed: false
+}
+]
